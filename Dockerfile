a74acaf753e0db99e2b79d2cafd920150471fe7f
FBmarko <turanmehmet2702@gmail.com>
2025-04-08 02:04:04 +0300

    Initial commit: Python Flask uygulaması eklendi

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,6 @@ FROM python:3.9-slim
 WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
-
-RUN pip install flask
-
 COPY app.py .
 EXPOSE 5000
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Merhaba, bu bir Python Flask uygulamasidir!"
+    return "Merhaba, bu bir Python Flask uygulamasýdýr!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
